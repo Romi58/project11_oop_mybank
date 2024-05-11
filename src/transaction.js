7ae69c113378672bd6d547abf96132d7152bd373
Romi58 <romaisanaz67@gmail.com>
2024-05-11 11:49:27 +0500

    transaction.js commit

---
+++ src/transaction.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Transaction = void 0;
+class Transaction {
+    constructor(amount) {
+        this.date = new Date();
+        this.amount = amount;
+    }
+    getFormattedDate() {
+        return this.date.toISOString();
+    }
+    getAmount() {
+        return this.amount;
+    }
+}
+exports.Transaction = Transaction;
